9393d85a41b06e69a7c4ed1803da18eaa833dbd7
Jack Chu <chuchuanbao@gmail.com>
2020-08-29 21:59:04 +0800

    feat(Jira): 增加Jira工具类

---
+++ src/main/java/com/godfather1103/util/JiraUtils.java
@@ -0,0 +1,32 @@
+package com.godfather1103.util;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+/**
+ * <p>Title:        Godfather1103's Github</p>
+ * <p>Copyright:    Copyright (c) 2020</p>
+ * <p>Company:      https://github.com/godfather1103</p>
+ *
+ * @author 作者: Jack Chu E-mail: chuchuanbao@gmail.com
+ * 创建时间：2020-08-29 21:53
+ * @version 1.0
+ * @since 1.0
+ * Jira相关工具类
+ */
+public class JiraUtils {
+    /**
+     * 获取待处理的任务列表<BR>
+     *
+     * @param server   服务器地址
+     * @param userName 用户名
+     * @param password 密码
+     * @return 相关列表
+     * @author 作者: Jack Chu E-mail: chuchuanbao@gmail.com
+     * 创建时间：2020-08-29 21:56
+     */
+    public static Map<String, String> getToDoList(String server, String userName, String password) {
+        // TODO 待增加相关获取逻辑
+        return new Hashtable<>(0);
+    }
+}
