e9b9a8984e5b069f012d67ebab74238af33def4c
Jack Chu <chuchuanbao@gmail.com>
2020-08-29 21:34:30 +0800

    feat(配置界面): 增加Jira相关参数的配置

--- src/main/java/com/godfather1103/ui/Settings.java
+++ src/main/java/com/godfather1103/ui/Settings.java
@@ -4,7 +4,6 @@ import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.options.Configurable;
-import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.ui.TextBrowseFolderListener;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import org.jetbrains.annotations.Nullable;
@@ -29,9 +28,15 @@ public class Settings implements Configurable {
     ResourceBundle bundle = ResourceBundle.getBundle("i18n/describe");
 
     public static final String PATH = "RuleConfFilePath";
+    public static final String JIRA_SERVER_ADDRESS = "JIRA_SERVER_ADDRESS";
+    public static final String JIRA_USERNAME = "JIRA_USERNAME";
+    public static final String JIRA_PASSWORD = "JIRA_PASSWORD";
 
     private TextFieldWithBrowseButton ruleConfFilePath;
     private JPanel rootPanel;
+    private JTextField jira_username;
+    private JPasswordField jira_password;
+    private JTextField jira_server;
 
     @Override
     public String getDisplayName() {
@@ -75,23 +80,51 @@ public class Settings implements Configurable {
     @Override
     public boolean isModified() {
         PropertiesComponent prop = PropertiesComponent.getInstance();
-        String storedPath = prop.getValue(PATH);
-        String uiPath = ruleConfFilePath.getText();
-        if (storedPath == null) {
-            storedPath = "";
+        String storedPath = showString(prop.getValue(PATH));
+        String storedJiraServerAddress = showString(prop.getValue(JIRA_SERVER_ADDRESS));
+        String storedJiraUserName = showString(prop.getValue(JIRA_USERNAME));
+        String storedJiraPassword = showString(prop.getValue(JIRA_PASSWORD));
+        String uiPath = showString(ruleConfFilePath.getText());
+        String uiAddress = showString(jira_server.getText());
+        String uiUserName = showString(jira_username.getText());
+        String uiPassword = showString(jira_password.getPassword());
+        return !storedPath.equals(uiPath)
+                || !storedJiraServerAddress.equals(uiAddress)
+                || !storedJiraUserName.equals(uiUserName)
+                || !storedJiraPassword.equals(uiPassword);
+    }
+
+    private String showString(String str) {
+        if (str == null || str.trim().length() == 0) {
+            return "";
+        } else {
+            return str.trim();
         }
-        return !storedPath.equals(uiPath);
+    }
+
+    private String showString(char[] str) {
+        if (str == null || str.length == 0) {
+            return "";
+        } else {
+            return new String(str);
+        }
     }
 
     @Override
-    public void apply() throws ConfigurationException {
+    public void apply() {
         PropertiesComponent prop = PropertiesComponent.getInstance();
-        prop.setValue(PATH, ruleConfFilePath.getText());
+        prop.setValue(PATH, showString(ruleConfFilePath.getText()));
+        prop.setValue(JIRA_SERVER_ADDRESS, showString(jira_server.getText()));
+        prop.setValue(JIRA_USERNAME, showString(jira_username.getText()));
+        prop.setValue(JIRA_PASSWORD, showString(jira_password.getPassword()));
     }
 
     @Override
     public void reset() {
         PropertiesComponent prop = PropertiesComponent.getInstance();
         ruleConfFilePath.setText(prop.getValue(PATH));
+        jira_server.setText(prop.getValue(JIRA_SERVER_ADDRESS));
+        jira_username.setText(prop.getValue(JIRA_USERNAME));
+        jira_password.setText(prop.getValue(JIRA_PASSWORD));
     }
 }
